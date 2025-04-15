389fe5da5f06f002e02066feb04d09b69f720629
Yassine Gherbi <y.gherbi.dev@gmail.com>
2025-04-15 08:35:33 +0200

    feat: add initial items data with types, prices, categories, and sizes

---
+++ src/data/items.ts
@@ -0,0 +1,111 @@
+import { type Item } from '@/types'
+
+const items: Item[] = [
+  {
+    type: 'grow_tent',
+    price: 100,
+    category: 'agriculture',
+    size: [2, 2],
+  },
+  {
+    type: 'packaging_station',
+    price: 100,
+    category: 'furniture',
+    size: [2, 4],
+  },
+  {
+    type: 'packaging_station_mk2',
+    price: 750,
+    category: 'furniture',
+    size: [2, 4],
+  },
+  {
+    type: 'bed',
+    price: 150,
+    category: 'furniture',
+    size: [3, 4],
+  },
+  {
+    type: 'coffee_table',
+    price: 120,
+    category: 'furniture',
+    size: [2, 3],
+  },
+  {
+    type: 'wooden_square_table',
+    price: 50,
+    category: 'none',
+    size: [2, 2],
+  },
+  {
+    type: 'metal_square_table',
+    price: 60,
+    category: 'none',
+    size: [2, 2],
+  },
+  {
+    type: 'floor_lamp',
+    price: 25,
+    category: 'furniture',
+    size: [1, 1],
+  },
+  {
+    type: 'TV',
+    price: 300,
+    category: 'furniture',
+    size: [2, 4],
+  },
+
+  {
+    type: 'small_storage_rack',
+    price: 30,
+    category: 'storage',
+    size: [1, 2],
+  },
+
+  {
+    type: 'medium_storage_rack',
+    price: 45,
+    category: 'storage',
+    size: [1, 3],
+  },
+
+  {
+    type: 'large_storage_rack',
+    price: 60,
+    category: 'storage',
+    size: [1, 4],
+  },
+  {
+    type: 'display_cabinet',
+    price: 250,
+    category: 'none',
+    size: [1, 2],
+  },
+  {
+    type: 'trash_can',
+    price: 25,
+    category: 'furniture',
+    size: [2, 2],
+  },
+  {
+    type: 'pot_sprinkler',
+    price: 200,
+    category: 'agriculture_tool',
+    size: [2, 2],
+  },
+  {
+    type: 'mixing_station',
+    price: 500,
+    category: 'furniture',
+    size: [2, 4],
+  },
+  {
+    type: 'soil_pourer',
+    price: 300,
+    category: 'agriculture_tool',
+    size: [2, 2],
+  },
+]
+
+export default items
