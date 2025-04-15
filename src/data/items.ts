7cec86adf1cc1c12e1c9af18af54a2c2883e9c1d
Yassine Gherbi <y.gherbi.dev@gmail.com>
2025-04-15 22:51:34 +0200

    feat: update items data and types to include new packaging and ingredient items

--- src/data/items.ts
+++ src/data/items.ts
@@ -1,111 +1,30 @@
 import { type Item } from '@/types'
 
 const items: Item[] = [
-  {
-    type: 'grow_tent',
-    price: 100,
-    category: 'agriculture',
-    size: [2, 2],
-  },
-  {
-    type: 'packaging_station',
-    price: 100,
-    category: 'furniture',
-    size: [2, 4],
-  },
-  {
-    type: 'packaging_station_mk2',
-    price: 750,
-    category: 'furniture',
-    size: [2, 4],
-  },
-  {
-    type: 'bed',
-    price: 150,
-    category: 'furniture',
-    size: [3, 4],
-  },
-  {
-    type: 'coffee_table',
-    price: 120,
-    category: 'furniture',
-    size: [2, 3],
-  },
-  {
-    type: 'wooden_square_table',
-    price: 50,
-    category: 'none',
-    size: [2, 2],
-  },
-  {
-    type: 'metal_square_table',
-    price: 60,
-    category: 'none',
-    size: [2, 2],
-  },
-  {
-    type: 'floor_lamp',
-    price: 25,
-    category: 'furniture',
-    size: [1, 1],
-  },
-  {
-    type: 'TV',
-    price: 300,
-    category: 'furniture',
-    size: [2, 4],
-  },
-
-  {
-    type: 'small_storage_rack',
-    price: 30,
-    category: 'storage',
-    size: [1, 2],
-  },
-
-  {
-    type: 'medium_storage_rack',
-    price: 45,
-    category: 'storage',
-    size: [1, 3],
-  },
-
-  {
-    type: 'large_storage_rack',
-    price: 60,
-    category: 'storage',
-    size: [1, 4],
-  },
-  {
-    type: 'display_cabinet',
-    price: 250,
-    category: 'none',
-    size: [1, 2],
-  },
-  {
-    type: 'trash_can',
-    price: 25,
-    category: 'furniture',
-    size: [2, 2],
-  },
-  {
-    type: 'pot_sprinkler',
-    price: 200,
-    category: 'agriculture_tool',
-    size: [2, 2],
-  },
-  {
-    type: 'mixing_station',
-    price: 500,
-    category: 'furniture',
-    size: [2, 4],
-  },
-  {
-    type: 'soil_pourer',
-    price: 300,
-    category: 'agriculture_tool',
-    size: [2, 2],
-  },
+  { type: 'baggie', category: 'packaging', price: 1, size: [1, 1] },
+  { type: 'jar', category: 'packaging', price: 3, size: [1, 1] },
+  { type: 'acid', category: 'ingredients', price: 40, size: [1, 1] },
+  { type: 'phosphorus', category: 'ingredients', price: 40, size: [1, 1] },
+  { type: 'gasoline', category: 'ingredients', price: 5, size: [1, 1] },
+  { type: 'soil', category: 'agriculture', price: 10, size: [1, 1] },
+  { type: 'long_life_soil', category: 'agriculture', price: 30, size: [1, 1] },
+  { type: 'extra_long_life_soil', category: 'agriculture', price: 60, size: [1, 1] },
+  { type: 'cuke', category: 'ingredients', price: 2, size: [1, 1] },
+  { type: 'banana', category: 'ingredients', price: 2, size: [1, 1] },
+  { type: 'paracetamol', category: 'ingredients', price: 3, size: [1, 1] },
+  { type: 'donut', category: 'ingredients', price: 3, size: [1, 1] },
+  { type: 'trash_bag', category: 'tools', price: 1, size: [1, 1] },
+  { type: 'viagra', category: 'ingredients', price: 4, size: [1, 1] },
+  { type: 'mouth_wash', category: 'ingredients', price: 4, size: [1, 1] },
+  { type: 'flu_medicine', category: 'ingredients', price: 5, size: [1, 1] },
+  { type: 'energy_drink', category: 'ingredients', price: 6, size: [1, 1] },
+  { type: 'motor_oil', category: 'ingredients', price: 6, size: [1, 1] },
+  { type: 'mega_bean', category: 'ingredients', price: 7, size: [1, 1] },
+  { type: 'chili', category: 'ingredients', price: 7, size: [1, 1] },
+  { type: 'battery', category: 'ingredients', price: 8, size: [1, 1] },
+  { type: 'iodine', category: 'ingredients', price: 8, size: [1, 1] },
+  { type: 'addy', category: 'ingredients', price: 9, size: [1, 1] },
+  { type: 'horse_semen', category: 'ingredients', price: 9, size: [1, 1] },
 ]
 
 export default items
---
+++ src/data/placeableItems.ts
@@ -0,0 +1,36 @@
+import { type Item } from '@/types'
+
+const placeableItems: Item[] = [
+  { type: 'bed', category: 'furniture', price: 150, size: [3, 5] },
+  { type: 'coffee_table', category: 'furniture', price: 120, size: [2, 4] },
+  { type: 'wooden_square_table', category: 'furniture', price: 50, size: [2, 2] },
+  { type: 'metal_square_table', category: 'furniture', price: 60, size: [2, 2] },
+  { type: 'floor_lamp', category: 'furniture', price: 25, size: [1, 1] },
+  { type: 'tv', category: 'furniture', price: 300, size: [4, 2] },
+  { type: 'small_storage_rack', category: 'storage', price: 30, size: [2, 1] },
+  { type: 'medium_storage_rack', category: 'storage', price: 45, size: [3, 1] },
+  { type: 'large_storage_rack', category: 'storage', price: 60, size: [4, 1] },
+  { type: 'display_cabinet', category: 'storage', price: 250, size: [3, 2] },
+  { type: 'trash_can', category: 'furniture', price: 25, size: [2, 2] },
+  { type: 'grow_tent', category: 'agriculture', price: 100, size: [2, 2] },
+  { type: 'pot_sprinkler', category: 'agriculture_tool', price: 200, size: [2, 1] },
+  { type: 'soil_pourer', category: 'agriculture_tool', price: 300, size: [2, 1] },
+  { type: 'suspension_rack', category: 'furniture', price: 40, size: [2, 2] },
+  { type: 'plastic_pot', category: 'agriculture', price: 20, size: [2, 2] },
+  { type: 'moisture_preserving_pot', category: 'agriculture', price: 50, size: [2, 2] },
+  { type: 'air_pot', category: 'agriculture', price: 120, size: [2, 2] },
+  { type: 'halogen_grow_light', category: 'lights', price: 40, size: [2, 2] },
+  { type: 'led_grow_light', category: 'lights', price: 80, size: [2, 2] },
+  { type: 'full_spectrum_grow_light', category: 'lights', price: 200, size: [2, 2] },
+  { type: 'mixing_station', category: 'furniture', price: 500, size: [4, 2] },
+  { type: 'mixing_station_mk2', category: 'furniture', price: 2000, size: [4, 2] },
+  { type: 'packaging_station', category: 'furniture', price: 100, size: [4, 2] },
+  { type: 'packaging_station_mk2', category: 'furniture', price: 750, size: [4, 2] },
+  { type: 'chemistry_station', category: 'furniture', price: 1000, size: [4, 2] },
+  { type: 'lab_oven', category: 'furniture', price: 1000, size: [4, 2] },
+  { type: 'drying_rack', category: 'furniture', price: 250, size: [3, 2] },
+  { type: 'brick_press', category: 'furniture', price: 800, size: [2, 2] },
+  { type: 'cauldron', category: 'none', price: 3000, size: [4, 4] },
+]
+
+export default placeableItems
